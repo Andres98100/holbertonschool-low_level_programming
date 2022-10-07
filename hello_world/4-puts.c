94de006deae732df202fd411aebe931f642a9919
andres98100 <andressolarte580@gmail.com>
2022-10-06 21:45:19 -0700

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+* main - Prints a specific text
+* Description: This function prints the text
+* that was given in the excercise
+* Return: zero to confirm everything went well
+*/
 int main(void)
 {
-puts("\"rogramming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
