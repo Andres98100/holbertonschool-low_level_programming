1f6648de6c56b593ba44a939c2bfae7cd9c115df
andres98100 <andressolarte580@gmail.com>
2022-10-06 22:27:22 -0700

    size

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+*main - print the size of there byts
+*Description: print the size oh there byts
+*Return: zero 
+*/
+int main(void)
+{
+printf("Size of a char: %s",sizeof(char));
+printf("Size of an int: %i",sizeof(int));
+printf("Size of a long int: %ld",sizeof(long int));
+printf("Size of a long long int: %lld",sizeof(long long int));
+printf("Size of a float: %f",sizeof(float); 
+return (0); 
+}
