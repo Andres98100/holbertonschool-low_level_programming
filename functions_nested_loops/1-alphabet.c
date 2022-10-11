535a33fedb602576d0090c165d29c6df9032c319
andres98100 <andressolarte580@gmail.com>
2022-10-11 06:54:54 -0700

    function

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+*main - main function
+*Description: show alphabet with functions
+*Return: zero
+*/
+void print_alphabet(void)
+{
+char l = 'a';
+while(l <= 'z')
+{
+printf("%c", l);
+l++;
+}
+}
+int main(void)
+{
+print_alphabet();
+return (0);
+}
