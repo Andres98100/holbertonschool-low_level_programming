99e08562701246fad4c7221a2152752b77e4830b
andres98100 <andressolarte580@gmail.com>
2022-10-11 14:29:40 -0700

    numbers

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -10,12 +10,7 @@ int print_sign(int n)
 if (n > 0)
 printf("+");
 return (1);
-else
- if (n == 0)
+if (n == 0)
 printf("0");
-return (0);
-else
- if (n < 0)
-printf("-");
-return (-1);
+return (0); 
 }
