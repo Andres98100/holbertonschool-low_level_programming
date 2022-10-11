4ef3d5bda6a7d28c94327a1559cf2252d08aed33
andres98100 <andressolarte580@gmail.com>
2022-10-10 19:50:39 -0700

    numbers singles

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+*main - function main
+*Description: print all numbers singles
+*Return: zero
+*/
+int main(void)
+{
+int n = 0;
+while (n < 10)
+{
+printf("%i\n", n);
+n++;
+}
+return (0);
+}
