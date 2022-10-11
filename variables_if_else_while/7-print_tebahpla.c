a4adec78739e690935d0715b96bbfe73cb0ce578
andres98100 <andressolarte580@gmail.com>
2022-10-10 20:51:45 -0700

    reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+*main - this is function main
+*Description: reverse alphabet
+*Return: zero
+*/
+int main(void)
+{
+char l = 'z';
+while (l <= 'a')
+{
+putchar(l);
+l--;
+}
+putchar('\n');
+return (0);
+}
