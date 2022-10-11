45300ad47c96b2e23d0834aee8f3aada5f6f676c
andres98100 <andressolarte580@gmail.com>
2022-10-10 20:23:52 -0700

    delete eq

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+*main - this is function main
+*Description: delete q, e
+*Return: zero
+*/
+int main(void)
+{
+char l = 'a';
+while (l < 'z')
+{
+  if (l != 'e' && l != 'q')
+    {
+      putchar(l);
+    }
+  l++;
+ }
+ putchar('\n');
+ return (0);
+}
