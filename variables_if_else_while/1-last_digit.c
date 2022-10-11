ad045210d5fe921f590aa645c4c2180b61102c16
andres98100 <andressolarte580@gmail.com>
2022-10-10 18:15:41 -0700

    if else

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,19 +13,19 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n == 0)
 {
-printf("%i is % i and is 0\n", n, n % 10);
+printf("Last digit of %i is % i and is 0\n", n, n % 10);
 }
 else
 {
 if (n > 5)
 {
-printf("%i is %i and is greater than 5\n", n, n % 10);
+printf("Last digit of %i is %i and is greater than 5\n", n, n % 10);
 }
 else
 {
 if (n < 6 && n > 0)
 {
-printf("%i is %i and is less than 6 and not 0\n", n, n % 10);
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
 }
 }
 }
