5dd32d4ef7b4114526e3dc664e45962f8a3f1f26
andres98100 <andressolarte580@gmail.com>
2022-10-10 18:55:15 -0700

    while

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+*main - function main
+*Description: print alphabet
+*Return: zero
+*/
+int main(void)
+{
+int l = 97;
+while (l < 122)
+putchar(l);
+putchar("\n");
+l++;
+return (0);
+}
