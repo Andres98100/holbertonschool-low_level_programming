9cccf52276e4aad9f2c321033ebac59349b1336e
andres98100 <andressolarte580@gmail.com>
2022-10-10 20:25:00 -0700

    delete eq

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -8,14 +8,14 @@
 int main(void)
 {
 char l = 'a';
-while (l < 'z')
+while (l <= 'z')
 {
-  if (l != 'e' && l != 'q')
-    {
-      putchar(l);
-    }
-  l++;
- }
- putchar('\n');
- return (0);
+if (l != 'e' && l != 'q')
+{
+putchar(l);
+}
+l++;
+}
+putchar('\n');
+return (0);
 }
