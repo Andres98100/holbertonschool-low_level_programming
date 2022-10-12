e735f4a16233357c3b62b3839f1dd637dd1670df
Andres98100 <andressolarte580@gmail.com>
2022-10-12 16:30:34 -0500

    table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*time_table - function
+*/
+void times_table(void){
+	int i = 0;
+	int l;
+	int c;
+	for (l = 0; l <= 9; l++)
+	{
+		for(c = 0; c <= 9;c++)
+		{
+			int result = l * c;
+			if (result > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+			else if (c == 0)
+			{
+				_putchar((result) + '0');
+			}
+			else if (result <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((result) + '0');
+			}
+		}
+		i++;
+		_putchar('\n');
+	}
+}
