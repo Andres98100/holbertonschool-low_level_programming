51b0b3601f4f7e27e38f77ea2441829e609ebdcb
andres98100 <andressolarte580@gmail.com>
2022-10-12 09:42:31 -0700

    24 hours

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -12,7 +12,7 @@ for (i = 0; i <= 23; i++)
 for (j = 0; j <= 59; j++)
 {
 putchar((i / 10) + '0');
-putchar((i % 10)+ '0');
+putchar((i % 10) + '0');
 putchar(':');
 putchar((j / 10) + '0');
 putchar((j % 10) + '0');
