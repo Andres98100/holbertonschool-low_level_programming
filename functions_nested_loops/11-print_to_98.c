c0dfaa49eadd3726705baa5be83edfbec3c0f891
andres98100 <andressolarte580@gmail.com>
2022-10-12 20:22:22 -0700

    98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include"main.h"
+/**
+*void print_to_98
+*@n1: variable
+*/
+void print_to_98(int n)
+{
+for (n1 ;n1 <= 98; n1++)
+{
+if (n1 != 98)
+{
+printf("%i", n1);
+_putchar(',');
+_putchar(' ');
+}
+else if (n1 == n1)
+{
+printf("%i", n1);
+}
+}
+for (n1; n1 >= 98; n1--)
+{
+if (n1 != 98)
+{
+printf("%i", n1);
+_putchar(',');
+_putchar(' ');
+}
+else if (n1 == n1)
+{
+printf("%i", n1);
+}
+}
+}
