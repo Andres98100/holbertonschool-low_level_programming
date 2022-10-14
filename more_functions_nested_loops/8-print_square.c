f034c495c9b7b5fc48a896337fa95339acc84e85
Andres98100 <andressolarte580@gmail.com>
2022-10-14 17:56:27 -0500

    sqaure

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include"main.h"
+/**
+*print_square - function
+*@size: variable
+*/
+void print_square(int size)
+{
+	int i;
+	int j;
+	
+	for (i = 1; i < size; i++)
+	{
+		for (j = 1; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
