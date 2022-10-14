96ea43e2533ab12bfb41aef995f89d927b42757c
Andres98100 <andressolarte580@gmail.com>
2022-10-13 23:30:44 -0500

    upper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*_isupper - function
+*@c: variable
+*Return: 1 or 0
+*/
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else if (c >= 'a' && c <= 'z')
+	{
+		return (0);
+	}
+}
---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,4 @@
+#define HEADER_FILE
+#define HEADER_FILE
+int _putchar.c(char c);
+#endif
