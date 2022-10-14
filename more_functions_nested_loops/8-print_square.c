69c67c90a6898664bbc8c67ccafb7377604fe3c2
Andres98100 <andressolarte580@gmail.com>
2022-10-14 17:58:41 -0500

    sqaure

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -9,9 +9,9 @@ void print_square(int size)
 	int i;
 	int j;
 	
-	for (i = 1; i < size; i++)
+	for (i = 1; i <= size; i++)
 	{
-		for (j = 1; j < size; j++)
+		for (j = 1; j <= size; j++)
 		{
 			_putchar('#');
 		}
