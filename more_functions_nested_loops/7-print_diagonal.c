fb1474f20aebbc483f2d6277705c7efc9e50331a
Andres98100 <andressolarte580@gmail.com>
2022-10-14 11:40:00 -0500

    \\

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -13,9 +13,13 @@ void print_diagonal(int n)
 	{
 		for (j = 0; j < n; j++)
 		{
-			if ( j == i)
+			if (j == i)
 			{
 				_putchar('\\');
+			}
+			else if (n <= 0)
+			{
+				_putchar('\n');
 			}
 			else
 			{
