854337b25c8f56793d1385c8754e22185e51397d
Andres98100 <andressolarte580@gmail.com>
2022-10-14 00:36:03 -0500

    wf loops

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include"main.h"
+/**
+*more_numbres - function
+*/
+void print_most_numbers(void)
+{
+	int w = 0;
+	int f;
+
+	while (i < 11)
+	{
+		for (f = 0; f < 15; f++)
+		{
+			if (f >= 10)
+			{
+				_putchar((f / 10) + '0');
+			}
+			_putchar((f % 10) + '0');
+		}
+			_putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -6,4 +6,5 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
 #endif
