4a69123e7b76c5e79c8568e30f904222cdd3f3c2
Andres98100 <andressolarte580@gmail.com>
2022-10-14 00:00:08 -0500

    more loops

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*_isdigit - function
+*@c: variable
+*Return: 1 or 0
+*/
+int _isdigit(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+	return (0);
+}
