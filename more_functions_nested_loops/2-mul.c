5d0344e6e7d3f45c209515a09e5c3f639ba00f71
Andres98100 <andressolarte580@gmail.com>
2022-10-14 00:12:07 -0500

    mul loops

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*mul - function
+*@a: variable
+*@b: variable
+Return: int
+*/
+int mul(int a, int b)
+{
+	int op;
+	op = a * b;
+	printf("%i", op);
+	return (op);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -3,4 +3,5 @@
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 #endif
