6cb7ca421f75a752e858f8014847daba30f17eaf
Andres98100 <andressolarte580@gmail.com>
2022-10-14 17:49:30 -0500

    diagonal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,16 +9,20 @@ void print_diagonal(int n)
 	int i;
 	int j;
 
-	for (i = 1; i < n; i++)
+	for (i = 1; i <= n; i++)
 	{
 		for (j = 1; j < i; j++)
 		{
 			if (i != 1)
+			{
 				_putchar(' ');
+			}
 		}
 		_putchar('\\');
 		_putchar('\n');
 	}
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 }
