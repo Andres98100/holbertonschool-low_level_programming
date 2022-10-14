3a8b7ca17ae3d7c4d2601d4ca9038258bbbaba34
Andres98100 <andressolarte580@gmail.com>
2022-10-14 00:22:52 -0500

    print loops

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include"main.h"
+/**
+*printf_numbers - function
+*/
+void print_numbers(void)
+{
+	int i = 0;
+	while (i < 10)
+	{
+		_putchar((i) + '0');
+		i++;
+	}
+	_putchar('\n');
+}
