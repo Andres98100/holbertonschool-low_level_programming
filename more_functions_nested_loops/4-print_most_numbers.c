c2abfa1a06ad06a38d6694e366d8d0947163e325
Andres98100 <andressolarte580@gmail.com>
2022-10-14 00:27:33 -0500

    0-9 loops

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include"main.h"
+/**
+*print_most_numbers - function
+*/
+void print_most_numbers(void)
+{
+	int i;
+	
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar((i) + '0');
+		}
+	}
+	_putchar('\n');
+}
