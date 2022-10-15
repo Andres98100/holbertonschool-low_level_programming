c14ef1902066f61dd2e29bc231a845aae799c31e
Andres98100 <andressolarte580@gmail.com>
2022-10-14 20:58:37 -0500

    fizzbuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include"main.h"
+/**
+*main - main function for print FizzBuzz
+*Description: print FizzBuzz
+*Return: zero
+*/
+int main(void)
+{
+	int i;
+	
+	for (i = 1; i <= 100; i++)
+	{
+		if(i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%i", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
