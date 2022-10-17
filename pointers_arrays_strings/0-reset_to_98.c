094302094f32b57688dbc2266b4db50587149de2
Andres98100 <andressolarte580@gmail.com>
2022-10-17 10:38:16 -0500

    pointer 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include"main.h"
+/**
+*reset_to_98 - function
+*@*n: variable
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+int _putchar(char c);
+void reset_to_98(int *n);
+#endif
