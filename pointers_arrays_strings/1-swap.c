6de53dce629afb28b52281afcaeee676ee518b73
Andres98100 <andressolarte580@gmail.com>
2022-10-17 10:45:06 -0500

    pointer swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include"main.h"
+/**
+*swap_int - function
+*@a: variable
+*@b: variable
+*/
+
+void swap_int(int *a, int *b)
+{
+	int swap;
+	swap = *a;
+	*a = *b;
+	*b = swap;
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,4 +2,5 @@
 #define HEADER_FILE
 int _putchar(char c);
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 #endif
