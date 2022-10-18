2e4fb6d1c3dd69603a9a22bb04b1d6832da7c6f2
Andres98100 <andressolarte580@gmail.com>
2022-10-18 13:03:58 -0500

    reverse string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include"main.h"
+/**
+*rev_string - function stdout
+*@s: variable
+*/
+void rev_string(char *s)
+{
+	int i = 0;
+	int j;
+	int pf;
+	int aux;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	pf = i - 1;
+	for (j = 0; j < i / 2; j++)
+	{
+		aux = s[j];
+		s[j] = s[pf];
+		s[pf] = aux;
+		aux--;
+	}
+}
