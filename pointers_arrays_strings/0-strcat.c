f4d99aaa88605fc63e06c212b32e13bd48250740
Andres98100 <andressolarte580@gmail.com>
2022-10-18 17:44:08 -0500

    concat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include"main.h"
+/**
+**_strcat - function print concat
+*@dest: variable
+*@src: variable
+*Return: char
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++);
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i + j] = '\0';
+	}
+	return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -10,4 +10,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
 #endif
