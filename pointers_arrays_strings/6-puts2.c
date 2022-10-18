033bdd12e428b0ff7256e8e7a7a53cf76f5a6f94
Andres98100 <andressolarte580@gmail.com>
2022-10-17 20:15:47 -0500

    pointer puts2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include"main.h"
+/**
+*puts2 - prints to stdout
+*@str: variable
+*/
+void puts2(char *str)
+{
+	int i;
+	int j;
+	while (c[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < i; j += 2)
+	{
+		putchar(c[j]);
+	}
+	putchar('\n');
+}
