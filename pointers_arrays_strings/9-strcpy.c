dcc2589e7f29418ab84a37da5b1372723e776a12
Andres98100 <andressolarte580@gmail.com>
2022-10-18 12:34:05 -0500

    array cpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include"main.h"
+/**
+**_strcpy - function that copy
+*@dest: variable
+*@src: variable
+*Return: char
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int j;
+
+	while (src[i] != '\0')
+	{
+		scr++;
+	}
+	for (j = 0; j <= i; j++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
