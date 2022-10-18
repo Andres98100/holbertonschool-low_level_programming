f8dd90ce8c33ff7ca3e64097fda06c727c264db3
Andres98100 <andressolarte580@gmail.com>
2022-10-18 13:36:45 -0500

    array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include"main.h"
+/**
+*print_array - function print n elements
+*@a: variable
+*@n: variable
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i < n -1)
+		{
+			printf("%i, ", a[i];
+		}
+		else
+		{
+		printf("%i\n", a[i]);
+		}
+	}
+	if (n <= 4)
+	{
+		printf("\n");
+	}
+}
