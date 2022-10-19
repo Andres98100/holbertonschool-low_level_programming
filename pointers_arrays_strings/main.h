d245e2435aaff733062c0c8fd33c02cc6772ff48
Andres98100 <andressolarte580@gmail.com>
2022-10-19 16:05:33 -0500

    strncpy

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -12,4 +12,6 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+void reverse_array(int *a, int n);
 #endif
