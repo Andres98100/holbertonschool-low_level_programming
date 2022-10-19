c48311b8ced25a8ec0faec3efbf8331110f461d2
Andres98100 <andressolarte580@gmail.com>
2022-10-19 11:03:53 -0500

    strncat

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -11,5 +11,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
-Prototype: char *_strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
 #endif
