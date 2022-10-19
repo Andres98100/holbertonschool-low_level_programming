07000bb323b2fa6f9dda00fa40547a010281695c
Andres98100 <andressolarte580@gmail.com>
2022-10-19 09:46:35 -0500

    concat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -10,7 +10,6 @@ char *_strcat(char *dest, char *src)
 {
 	int i = 0;
 	int j = 0;
-	char *aux;
 
 	while (dest[i] != '\0')
 	{
@@ -21,6 +20,6 @@ char *_strcat(char *dest, char *src)
 		dest[i + j] = src[j];
 		j++;
 	}
-	aux[i + j] = '\0';
-	return (aux);
+	dest[i + j] = '\0';
+	return (dest);
 }
