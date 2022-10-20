89f1c25df542135173d76f548837a69b73737e6e
Andres98100 <andressolarte580@gmail.com>
2022-10-19 22:04:59 -0500

    compare

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -12,6 +12,7 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 #endif
