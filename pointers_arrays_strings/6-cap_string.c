a68b1f34026a19d9041af9233796a72876151b7f
Andres98100 <andressolarte580@gmail.com>
2022-10-20 13:20:51 -0500

    first uppercase

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include"main.h"
+/**
+**_capstring - function show the first letter in uppercase
+*@c: variable
+*Return: char
+*/
+char *cap_string(char *)
+{
+	int i = 0;
+
+	while (i != '\0')
+	{
+		if (i == 0)
+		{
+			if (c[i] >= 'a' && c[i] <= 'z')
+			{
+				c[i] -= 32;
+			}
+		}
+		if (c[i] == ' ')
+		{
+			i++;
+			if ( c[i] >= 'a' && c[i] <= 'z')
+			{
+				c[i] -= 32;
+			}
+		}
+		else
+		{
+			if (c[i] >= 'A' && c[i] <= 'Z')
+			{
+				c[i] += 32;
+			}
+		}
+		i++;
+	}
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -16,4 +16,5 @@ int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 char *string_toupper(char *c);
+char *cap_string(char *c);
 #endif
