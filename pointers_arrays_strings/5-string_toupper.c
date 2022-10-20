b986f064fa329d971e537ea8de5661222a33addb
Andres98100 <andressolarte580@gmail.com>
2022-10-20 12:40:03 -0500

    uppercase

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include"main.h"
+/**
+*string_toupper - function 
+*@c: variable
+*Return: char
+*/
+char *string_toupper(char *c)
+{
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		if (c[i] >= 'a' && c[i] <= 'z')
+		{
+			c[i] -= 32;
+		}
+		i++;
+	}
+	return (c);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -15,4 +15,5 @@ char *_strncat(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
+char *string_toupper(char *c);
 #endif
