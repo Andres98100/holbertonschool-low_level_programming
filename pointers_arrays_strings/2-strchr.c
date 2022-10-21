c1be31f3a50b0bff72e1222cab5bec4462668d3e
Andres98100 <andressolarte580@gmail.com>
2022-10-21 10:55:02 -0500

    return strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include"main.h"
+/**
+**_strchr - function locates a char in a string
+*@s: variable
+*@c: variable
+*Return: char
+*/
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; i <= c; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		if (s[i] == c)
+		{
+			return (0);
+		}
+	}
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -20,4 +20,5 @@ char *cap_string(char *c);
 char *leet(char *);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 #endif
