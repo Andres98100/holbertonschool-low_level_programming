4c31e45eccdf24ab379bb7a4e895b079f34fb713
Andres98100 <andressolarte580@gmail.com>
2022-10-21 11:37:36 -0500

    substring

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include"main.h"
+/**
+*int_strspn - function get the length of a substring
+*@s: variable pointer
+*@accept: variable pointer
+*Return: unsigned
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0;
+	int j = 0;
+
+	while s[i] != '\0')
+	{
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == accept[i])
+			{
+				break;
+			}
+			j++;
+		}
+		if (accept[j] == '\0')
+		{
+			break;
+		}
+		i++;
+	}
+	return (i);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -21,4 +21,5 @@ char *leet(char *);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 #endif
