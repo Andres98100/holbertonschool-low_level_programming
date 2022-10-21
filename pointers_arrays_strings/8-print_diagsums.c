53c2e3bdce9936229d38d646a0740aef30974d30
Andres98100 <andressolarte580@gmail.com>
2022-10-21 18:34:25 -0500

    diag

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -15,18 +15,12 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size; i++)
 	{
-		if (i == i)
-		{
-			result += a[i * (size + 1)];
-		}
+		result += a[i * (size + 1)];
 	}
 	for (j = 0; j < size; j++)
 	{
 		lp = (size - j) - 1;
-		if (j == j)
-		{
-			result2 = a[(j * size) + (n)];
-		}
+		result2 = a[(j * size) + (lp)];
 	}
 	printf("%d, %d\n", result, result2);
 }
