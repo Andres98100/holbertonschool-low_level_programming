f3d57260176b50489c8918a1821ebe4f1ff26469
Andres98100 <andressolarte580@gmail.com>
2022-10-21 14:48:00 -0500

    chees

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -9,9 +9,9 @@ void print_chessboard(char (*a)[8])
 	int i;
 	int j;
 
-	for (i = 0; i <= 8; i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (j = 0; j <= 8; j++)
+		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
 		}
