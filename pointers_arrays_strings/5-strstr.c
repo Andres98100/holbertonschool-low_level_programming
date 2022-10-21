535bd2a20220a7cde9f9379973a53e18df0daa6b
Andres98100 <andressolarte580@gmail.com>
2022-10-21 13:53:07 -0500

    string

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include"main.h"
+/**
+**_strstr - funciton locate a substring
+*@haystack: variable pointer
+*@needle: variable pointer
+*Return: char
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+	
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] == needle[j])
+			{
+				return (needle);
+			}
+		}
+		if (haystack[i] == needle[j])
+		{
+			return (needle);
+		}
+	}
+	return (0);
+}
