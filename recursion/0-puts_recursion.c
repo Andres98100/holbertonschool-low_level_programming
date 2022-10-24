3d8f4aeae0c3a4ec32b2e6d3b3424b69c7d1708d
Andres98100 <andressolarte580@gmail.com>
2022-10-24 14:09:49 -0500

    string

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+*_puts_recursion - function print a string
+*@s: variable pointer
+*/
+void _puts_recursion(char *s)
+{
+	if (s != '\0')
+	{
+		printf("%s", s);
+	}
+	s++;
+}
