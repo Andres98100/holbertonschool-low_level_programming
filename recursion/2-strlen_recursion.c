7db70d0da9237f4512ed008dfe32c89c661b50f3
Andres98100 <andressolarte580@gmail.com>
2022-10-24 16:28:58 -0500

    length

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+*_strlen_recursion - function print the length of a string
+*@s: variable pointer
+*Return: int
+*/
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (s[len])
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
