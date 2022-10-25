d99d6ddd8918d4c613ce27cd8da4d06ce5c2b185
Andres98100 <andressolarte580@gmail.com>
2022-10-25 11:41:20 -0500

    sqrt

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+/**
+*_sqrt_recursion - function print of the sqrt the a number
+*@n: variable
+*Return: int
+*/
+int aux_sqrt(int n, int i)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else if (i * i == n)
+	{
+		return (n);
+	}
+	else
+	{
+		return (aux_sqrt(n, i + 1));
+	}
+}
+
+int _sqrt_recursion(int n)
+{
+	return (aux_sqrt(n, 1));
+}
