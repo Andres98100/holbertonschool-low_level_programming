954ebab5299dfcee636acf4ef61c3b79a3a769dd
Andres98100 <andressolarte580@gmail.com>
2022-10-27 17:54:15 -0500

    add

---
+++ argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+*main - function main
+*@argc: variable int
+@argv: variable matriz
+*Return: zero
+*/
+int main(int argc, char **argv)
+{
+	int n1;
+	int n2;
+	int add = 0;
+
+	for (n1 = 0; n1 < argc; n1++)
+	{
+		for (n2 = 0; argv[n1][n2] != '\0'; n2++)
+		{
+			if ((argv[n1][n2] < '0') || (argv[n1][n2]) > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[n1]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
