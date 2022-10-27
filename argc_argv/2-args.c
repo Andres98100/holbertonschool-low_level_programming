f97f7aa4f262eb91dbd81b35a1882b7bf8b88d70
Andres98100 <andressolarte580@gmail.com>
2022-10-27 16:56:13 -0500

    arguments

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+*main - function main
+*@argc: variable int
+*@argv: variable matriz
+*Return: zero
+*/
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
