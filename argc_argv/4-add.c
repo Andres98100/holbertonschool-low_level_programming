2255ba1a3a6a1701d671f5870dcffa113aa79617
Andres98100 <andressolarte580@gmail.com>
2022-10-27 17:57:21 -0500

    add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -16,13 +16,13 @@ int main(int argc, char **argv)
 	{
 		for (n2 = 0; argv[n1][n2] != '\0'; n2++)
 		{
-			if ((argv[n1][n2] < '0') || (argv[n1][n2] > '9'))
+			if ((argv[n1][n2] < '0') || argv[n1][n2] > '9')
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-		add += atoi(argv[n1]);
+		add += (atoi(argv[n1]));
 	}
 	printf("%d\n", add);
 	return (0);
