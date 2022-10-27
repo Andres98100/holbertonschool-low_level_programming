57b0e46954ce5f7ddbdfd6d251f760cdfa493e54
Andres98100 <andressolarte580@gmail.com>
2022-10-27 17:21:00 -0500

    mul

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+*main - function main
+*@argc: variable int
+*@argv: variable matriz
+*Return: zero
+*/
+int main(int argc, char **argv)
+{
+	int n1 = 0;
+	int n2 = 0;
+	int result = 0;
+
+	if (argc > 1)
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		result = n1 * n2;
+		printf("%d\n", result);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
