c2c1af922c42bba4e6af25871c950fab763343a8
Andres98100 <andressolarte580@gmail.com>
2022-10-27 16:13:56 -0500

    number

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+*main - function main
+*@argc: variable int
+*@argv: variable matriz
+*Return: zero
+*/
+int main(int argc, char **argv)
+{
+	printf("%d", argc);
+	return (0);
+}
