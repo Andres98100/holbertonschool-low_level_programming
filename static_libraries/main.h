a0774c0780483367f673b58ee0f824e5ad0dc6db
Andres98100 <andressolarte580@gmail.com>
2022-10-27 12:32:46 -0500

    static

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,5 +1,6 @@
 #ifndef HEADER_FILE
 #define HEADER_FIlE
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -20,4 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
 #endif
