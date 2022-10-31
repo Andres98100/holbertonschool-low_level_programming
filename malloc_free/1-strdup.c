2ddcb1a2d537e45030b445a03ca897a0387cbcc2
Andres98100 <andressolarte580@gmail.com>
2022-10-31 15:52:25 -0500

    duplicated

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -30,17 +30,3 @@ char *_strdup(char *str)
 	}
 	return (arr);
 }
-int main(void)
-{
-	  char *s;
-
-		s = _strdup("Duplicated");
-		if (s == NULL)
-		{
-			printf("failed to allocate memory\n");
-			return (1);
-		}
-		printf("%s\n", s);
-		free(s);
-		return (0);
-}
