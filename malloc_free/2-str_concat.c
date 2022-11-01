960d31827bac23cc1d4ee5848d5f95744529854c
Andres98100 <andressolarte580@gmail.com>
2022-11-01 08:58:42 -0500

    cat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -8,18 +8,18 @@
 *@s2: variable pointer char
 *Return: pointer char
 */
-char *str_concat(__attribute__((unused)) char *s1, char *s2)
+char *str_concat(char *s1, char *s2)
 {
 	char *cat = NULL;
 
 	if (s1 == "")
-	{
+
 		return (NULL);
-	}
+
 	if (s2 == "")
-	{
+
 		return (NULL);
-	}
+
 	cat = malloc(sizeof(char) * (strlen(s1) + strlen(s2)) + 1);
 	strcat(cat, s1);
 	strcat(cat, s2);
