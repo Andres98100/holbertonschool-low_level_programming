4c040d52f8eb05e963f2acc9e93906119ef4d753
Andres98100 <andressolarte580@gmail.com>
2022-11-01 10:22:45 -0500

    cat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,17 +10,39 @@
 */
 char *str_concat(char *s1, char *s2)
 {
+	int len = 0;
+	int len2 = 0;
+	int i, j;
 	char *cat = NULL;
 
 	if (s1 == "")
-
+	{
 		return (NULL);
-
+	}
 	if (s2 == "")
-
+	{
 		return (NULL);
-
-	cat = malloc(sizeof(char) * (strlen(s1) + strlen(s2)) + 1);
-	cat = strcat(s1, s2);
+	}
+	len = strlen(s1);
+	len2 = strlen(s2);
+	cat = malloc((len + len2) + 1);
+	if (cat == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < len)
+	{
+		cat[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (j < len2)
+	{
+		cat[i] = s2[j];
+		i++;
+		j++;
+	}
+	cat[i] = '\0';
 	return (cat);
 }
