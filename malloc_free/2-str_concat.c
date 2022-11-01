9b178d0f749770c2f5dcecd6a1e3bd79f9271ad0
Andres98100 <andressolarte580@gmail.com>
2022-11-01 16:35:14 -0500

    grid

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,11 +15,7 @@ char *str_concat(char *s1, char *s2)
 	int i, j;
 	char *cat = NULL;
 
-	if (s1 == NULL)
-	{
-		return (NULL);
-	}
-	if (s2 == NULL)
+	if (s1 == NULL || s2 == NULL)
 	{
 		return (NULL);
 	}
