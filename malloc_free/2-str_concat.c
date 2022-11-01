dd73eab001387610e9d2e3bcdf906655738312e7
Andres98100 <andressolarte580@gmail.com>
2022-11-01 16:37:44 -0500

    grid

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,11 +17,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		return ("");
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		return ("");
+		s2 = "";
 	}
 	len = strlen(s1);
 	len2 = strlen(s2);
