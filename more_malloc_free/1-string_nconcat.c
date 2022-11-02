40fe8c75df0a0335564f4235c5c10675e77ff895
Andres98100 <andressolarte580@gmail.com>
2022-11-02 09:04:47 -0500

    cat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -11,20 +11,20 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int len = 0;
-	unsigned int len2 = 0;
-	int i;
-	unsigned int j;
+	int len, i;
+	unsigned int len2, j;
 	char *cat = NULL;
-	
-	if (s1 == NULL)
+
+	if (s1 == 0)
 	{
 		s1 = "";
 	}
-	if (s2 == NULL)
+	if (s2 == 0)
 	{
 		s2 = "";
 	}
+	len = strlen(s1);
+	len2 = strlen(s2);
 	if (n > len2)
 	{
 		cat = malloc((len + len2) + 1);
@@ -37,17 +37,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
+	i = 0;
 	while (i < len)
 	{
 		cat[i] = s1[i];
 		i++;
 	}
-	while (j < len2)
+	j = 0;
+	while (j < n && j < len2)
 	{
 		cat[i] = s2[j];
 		i++;
-		j++;
+		j++,
 	}
-	cat[i] = '\0';
+	cat = '\0';
 	return (cat);
 }
