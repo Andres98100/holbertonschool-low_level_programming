9c94aad33a918ba4587a0856cf612ab73e7e9372
Andres98100 <andressolarte580@gmail.com>
2022-11-02 08:44:32 -0500

    cat

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+*string_nconcat - funtion concat two string
+*@s1: variable
+*@s2: variable
+*@n: variable
+*Return: char
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int len = 0;
+	int len2 = 0;
+	int i, j;
+	char *cat = NULL;
+	
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	if (n > len2)
+	{
+		cat = malloc((len + len2) + 1);
+	}
+	else
+	{
+		cat = malloc((len + n) + 1);
+	}
+	if (cat == NULL)
+	{
+		return (NULL);
+	}
+	while (i < len)
+	{
+		cat[i] = s[i];
+		i++;
+	}
+	while (j < len2)
+	{
+		cat[i] = s2[j];
+		i++;
+		j++;
+	}
+	cat[i] = '\0';
+	return (cat);
+}
---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*_calloc - function allocates memory
+*@nmemb: variable
+*@size: variable
+*Return: zero
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	 
