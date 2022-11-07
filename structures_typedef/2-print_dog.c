8762273baae919045df2a11225c2874bcf848c6d
Andres98100 <andressolarte580@gmail.com>
2022-11-07 17:39:22 -0500

    print struct

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - function print the struct dog
+ * @d: variable struct
+ * Retrun: zero
+ */
+void print_dog(struct dog *d)
+{
+	int i;
+
+	if (d)
+	{
+		d->name = name;
+		d-> = age;
+		d->owner = owner;
+	}
+	for (i = 0; i < d; i++)
+	{
+		printf("Name: %s\n", d[i].name);
+		printf("Age: %f\n", d[i].age);
+		printf("Owner: %s", d[i].owner);
+	}
+}
