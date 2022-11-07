ec0724811273ad7e5b8f52229ecf8759b8f53fc8
Andres98100 <andressolarte580@gmail.com>
2022-11-07 10:03:26 -0500

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * struct struct dog - printf the elements
+ *@name: variable
+ *@age: variable
+ *@owner: variable
+ *Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
