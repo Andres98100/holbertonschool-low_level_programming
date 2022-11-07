8359595ced581ecbff85686fa0595a52fe446e58
Andres98100 <andressolarte580@gmail.com>
2022-11-07 18:00:09 -0500

    print struct

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 			printf("Name: nil\n");
 		else
 			printf("Name: %s\n", d->name);
-		printf("Age: %f", d->age);
+		printf("Age: %f\n", d->age);
 		if (d->owner == NULL)
 			printf("Owner: nill\n");
 		else
