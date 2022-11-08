8bc2b53f6a19c8f8bfc3369f75f3bc87f7c920b0
Andres98100 <andressolarte580@gmail.com>
2022-11-08 12:39:23 -0500

    name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -11,16 +11,17 @@ void print_name(char *name, void (*f)(char *))
 {
 	int i;
 
+	if (name)
+	{
+		for (i = 0; name[i]; i++)
+		{
+			f(&name[i]);
+		}
+	}
 	name = malloc(sizeof(name) + 1);
 	if (name != NULL)
 	{
 		free(name);
 	}
-	if (name)
-	{
-		for (i = 0; name[i]; i++)
-		{
-			f(name[i]);
-		}
-	}
+	return;
 }
