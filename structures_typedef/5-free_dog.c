190e69bc5da8dcd95976a59321a5ad9329429f63
Andres98100 <andressolarte580@gmail.com>
2022-11-08 11:01:26 -0500

    free

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - function free the dog
+ * @d: variable struct
+ * Return: zero
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
