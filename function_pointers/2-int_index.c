0ea94e76af91366c7679aa6e50216cdbaddbc1f0
Andres98100 <andressolarte580@gmail.com>
2022-11-08 18:28:29 -0500

    array intger

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -11,17 +11,20 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-
+	
+	if (size <= 0)
+	{
+		return (-1);
+	}
 	if (array || cmp)
 	{
 		for (i = 0; i < size; i++)
 		{
-			cmp(array[i]);
-		}
-		if (size <= 0)
-		{
-			return (-1);
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
 		}
 	}
-	return (cmp);
+	return (i);
 }
