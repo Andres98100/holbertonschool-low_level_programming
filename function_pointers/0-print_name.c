19d07acb47212547716dfd5925a05edeee30ceb4
Andres98100 <andressolarte580@gmail.com>
2022-11-08 11:51:31 -0500

    name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "function_pointer.h"
+/**
+ * print_name - function print the name
+ * @name: variable char
+ * @f: variable void
+ * Return: zero
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	int i;
+	if (name)
+	{
+		for (i = 0; name[i]; i++)
+		{
+			f(&name[i]);
+		}
+	}
+}
---
+++ function_pointers/function_pointer.h
@@ -0,0 +1,5 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+#endif
