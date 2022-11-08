1c3e4a7afa564a68b9ba84be06055b1365d9d4db
Andres98100 <andressolarte580@gmail.com>
2022-11-08 10:19:11 -0500

    save

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
+/**
+ * new_dog - funtion save a copy
+ * @name: variable char
+ * @age: variable float
+ * @owner: variable char
+ * Return: struct
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newDog;
 
+
 	newDog = malloc(sizeof(newDog));
-	if (newDog != NULL)
+	if (newDog == NULL)
 		return (NULL);
 	if (newDog != NULL)
 	{
