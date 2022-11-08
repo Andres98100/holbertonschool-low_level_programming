66ba42238113d7dce96f3dea774f61d7cc4e6b34
Andres98100 <andressolarte580@gmail.com>
2022-11-08 10:38:36 -0500

    save

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,7 +12,13 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newDog;
+	int lenName;
+	int lenOwner;
+	int i;
+	int j;
 
+	lenName = strlen(name);
+	lenOwner = strlen(owner);
 	newDog = malloc(sizeof(dog_t));
 	if (newDog == NULL)
 		return (NULL);
@@ -23,6 +29,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 		newDog->owner = owner;
 	}
 	newDog->name = malloc(strlen(name) + 1);
+	if (newDog->name == NULL)
+	{
+		free(newDog->name);
+		free(newDog);
+		return (NULL);
+	}
 	newDog->owner = malloc(strlen(owner) + 1);
+	if (newDog->owner == NULL)
+	{
+		free(newDog->owner);
+		free(newDog);
+		return (NULL);
+	}
+	for (i = 0; i < lenName; i++)
+	{
+		newDog->name[i] = name[i];
+	}
+	for (j = 0;  < lenOwner; j++)
+	{
+		newDog->owner = owner[i];
+	}
 	return (newDog);
 }
