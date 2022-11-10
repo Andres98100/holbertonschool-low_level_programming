66c67efc30c4d450c000d5ffcb088df9bee2400a
Andres98100 <andressolarte580@gmail.com>
2022-11-10 17:38:14 -0500

    sum numbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -15,7 +15,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ptr, n);
 	for (i = 0; i < n; i++)
 	{
-		if (separator && i < n-1)
+		if (separator && i < n - 1)
 			printf("%d%s", va_arg(ptr, int), separator);
 		else
 			printf("%d", va_arg(ptr, int));
