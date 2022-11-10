fd94bdc17bfc28675e3a10d318dea8e7bc1c5f88
Andres98100 <andressolarte580@gmail.com>
2022-11-09 19:13:30 -0500

    op function

---
+++ function_pointers/3-main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+int main(int argc. *argv[])
+{
+	int num1, num2;
+	int (*getopfun)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exir(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	getopfun = get_op_func(argv[2]);
+	getopfun(num1, num2);
+	if (getopfun == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	else
+	{
+		printf("%d\n", getopfun(num1, num2));
+	}
+}
