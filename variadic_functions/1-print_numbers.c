ae23f94b025b3a4ce1abca40eeeead85c2d364eb
Andres98100 <andressolarte580@gmail.com>
2022-11-10 09:48:37 -0500

    separator

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	 for (i = 0; i < n; i++)
 		{
 			value = va_arg(ptr, int);
-			if (separator[i] < value)
+			if (i < value)
 				printf("%c %d", separator[i], value);
 		}
 	}
