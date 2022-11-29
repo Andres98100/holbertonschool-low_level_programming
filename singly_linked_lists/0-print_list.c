6e588f380491fdc6bd1d9f025be41a598292824b
Andres98100 <andressolarte580@gmail.com>
2022-11-29 11:01:33 -0500

    print

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,21 +3,22 @@
 /**
  * print_list - function print linkedlist
  * @h: variable struct
- * return: size_t
+ * Return: size_t
  */
 
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-	
-	while(h)
+
+	while (h)
 	{
 		 if (h->str == NULL)
-			 printf("[0] (nil)");
+			 printf("[0] (nil)\n");
 		 else
-			 printf("[%d] [%s]\n", h->len, h->str);
+			 printf("[%d] %s\n", h->len, h->str);
 		 h = h->next;
 		 count++;
 	}
 	return (count);
 }
+
