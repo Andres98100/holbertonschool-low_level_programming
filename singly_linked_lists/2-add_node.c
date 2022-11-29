14c6eb82d244e92215a1b3c7e2a5544e0749744a
Andres98100 <andressolarte580@gmail.com>
2022-11-29 14:53:26 -0500

    add

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_node - function add elements
+ * @head: variable struct
+ * @str: variable pointer char
+ */
+
+lists_t *add_node(list_t **head, const char *str)
+{
+	lists_t *new;
+	int count = 0;
+	int lenstr;
+	lenstr = strlen(str);
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	new->str = strdup(str);
+	new->len = lenstr;
+	new->next = head;
+	head = new;
+	return (new);
+}
