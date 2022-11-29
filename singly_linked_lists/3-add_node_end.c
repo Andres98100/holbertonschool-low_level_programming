d1f7141a60a5196f7242fde4972ef7065d0458c4
andres98100 <andressolarte580@gmail.com>
2022-11-29 18:56:34 -0500

    add in the tail

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * add_node_end - function
+ * @head: variable struct
+ * @str: variable pointer char
+ * Return: struct
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+    list_t *new;
+    int lenstr;
+    list_t *last = *head;
+
+    lenstr = strlen(str);
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+        return (NULL);
+    new->str = strdup(str);
+    new->len = lenstr;
+    new->next = NULL;
+    if ((*head) == NULL)
+    {
+        (*head) = new;
+        return (new);
+    }
+
+    while (last->next != NULL)
+        last = last->next;
+    last->next = new;
+    return (new);
+}
