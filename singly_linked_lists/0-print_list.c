2218e4fc3c00cac14798b5f436619ba2b7f75a5e
Andres98100 <andressolarte580@gmail.com>
2022-11-29 10:55:58 -0500

    print

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -13,7 +13,7 @@ size_t print_list(const list_t *h)
 	while(h)
 	{
 		 if (h->str == NULL)
-			 printf("[0] (nil)");
+			 printf("[0] (nil)", h->len, h->str);
 		 else
 			 printf("%d %s\n", h->len, h->str);
 		 h = h->next;
