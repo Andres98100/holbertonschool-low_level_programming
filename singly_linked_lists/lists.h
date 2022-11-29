f2894fc7c182facdf36dd7c0d7199d9c4f6496d9
Andres98100 <andressolarte580@gmail.com>
2022-11-29 11:10:17 -0500

    print

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -12,12 +12,13 @@
  *
  * Description: singly linked list node structure
  */
- typedef struct list_s
+typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-}list_t;
+} list_t;
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
