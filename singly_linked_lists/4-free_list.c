d0e380d71f466c715c8defdf61b8a69799ea8c17
andres98100 <andressolarte580@gmail.com>
2022-11-29 19:33:18 -0500

    free

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -9,9 +9,8 @@
 void free_list(list_t *head)
 {
 	while (head == NULL)
-	{
-		free(head->next);
-		free(head->str);
-		free(head);
-	}
+		return;
+	free(head->next);
+	free(head->str);
+	free(head);
 }
