12f7f0663c5abd9f3a48d0807c266bca36a4b6b0
andres98100 <andressolarte580@gmail.com>
2022-11-29 19:02:58 -0500

    add in the tail

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -7,25 +7,25 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new;
-    int lenstr;
-    list_t *last = *head;
+	list_t *new;
+	int lenstr;
+	list_t *last = *head;
 
-    lenstr = strlen(str);
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-        return (NULL);
-    new->str = strdup(str);
-    new->len = lenstr;
-    new->next = NULL;
-    if ((*head) == NULL)
-    {
-        (*head) = new;
-        return (new);
-    }
+	lenstr = strlen(str);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = lenstr;
+	new->next = NULL;
+	if ((*head) == NULL)
+	{
+		(*head) = new;
+		return (new);
+	}
 
-    while (last->next != NULL)
-        last = last->next;
-    last->next = new;
-    return (new);
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new;
+	return (new);
 }
