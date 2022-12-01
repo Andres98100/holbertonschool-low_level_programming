41726f3b47fd3dd3a774fe5326695dfed7790be0
andres98100 <andressolarte580@gmail.com>
2022-12-01 18:01:10 -0500

    update nth

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * get_nodeint - function return the nth node
+ * get_nodeint_at_index - function return the nth node
  * @head: variable struct
  * @index: variable unsigned int
  * Return: structure
@@ -18,7 +18,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		len++;
 		aux = aux->next;
 	}
-	if (!head || !index)
+	if (!head)
 		return (NULL);
 	if (index > len)
 		return (NULL);
