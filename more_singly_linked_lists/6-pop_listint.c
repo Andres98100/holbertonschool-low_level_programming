aa0feac4f9ec3bc65bef670603fe4c5b4220f281
andres98100 <andressolarte580@gmail.com>
2022-12-01 17:05:14 -0500

    delete the first

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -8,24 +8,12 @@
 
 int pop_listint(listint_t **head)
 {
-	listint_t *aux = *head;
-	listint_t *prev_node;
+	listint_t *new = *head;
+	int count;
 
-	if (!head)
-		head = &aux;
-	if (!*head)
-		*head = aux;
-	else
-	{
-		while (aux)
-		{
-			if (aux->n == *head)
-				head* = aux->next;
-			else
-				prev_node->next = aux->next;
-			prev_node = aux;
-			aux = aux->next;
-		}
-	}
-	return (*head);
+	if (!head || !*head)
+		return (0);
+	count = (*head)->n;
+	*head = new->next;
+	return (count);
 }
