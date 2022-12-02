4b0185ee1bdf14b9f19a56ac74e60a5dcad265ab
andres98100 <andressolarte580@gmail.com>
2022-12-02 15:21:04 -0500

    print binary

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -4,11 +4,11 @@
  * print_binary - function print binary
  * @n: variable long int
  * Return: void
-*/
+ */
 
 void print_binary(unsigned long int n)
 {
-    if (n)
-        print_binary(n >> 1);
-    _putchar((n & 1) + '0');
+	if (n && n >> 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
