296634a5a0652742c8261ffeb3287446c9f3c851
andres98100 <andressolarte580@gmail.com>
2022-12-02 17:16:50 -0500

    delete node

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * delete_noteint - dunction delete a node
+ * @head: variable struct
+ * @index: variable int
+ * Return: int
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *new, *aux;
+	unsigned int i = 0;
+	unsigned int len = 0;
+
+	if (!*head)
+		return (-1);
+	aux = *head;
+	while (aux)
+	{
+		len++;
+		aux = aux->next;
+	}
+	if (len < index)
+		return (-1);
+	aux = *head;
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(aux);
+		return (1);
+	}
+	while (i < index - 1)
+	{
+		aux = aux->next;
+		i++;
+	}
+	new = aux->next;
+	aux->next = new->next;
+	free(new);
+	return (1);
+}
