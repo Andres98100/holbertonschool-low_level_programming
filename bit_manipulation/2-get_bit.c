aef12d00345928856c38fc0357a5c5e2b8b1f0bf
andres98100 <andressolarte580@gmail.com>
2022-12-02 17:28:33 -0500

    get bit

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * get_bin - function return a bit
+ * get_bit - function return a bit
  * @n: variable long int
  * @index: variabble int
  * Return: int
-*/
+ */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-    int bit = 0;
-    
-    bit = (n >> index) & 1;
-    return (bit);
+	int bit = 0;
+
+	bit = (n >> index) & 1;
+	return (bit);
 }
