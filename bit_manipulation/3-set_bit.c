1c0943b7d745f93ad78201b14eeb0bc7e7762c63
andres98100 <andressolarte580@gmail.com>
2022-12-02 18:33:03 -0500

    sets of a bit

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_bit - function sets the value of a bit
+ * @n: variable long int
+ * @index: variable int
+ * Return: int
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (!n)
+		return (-1);
+	*n = *n | (1lu << index);
+	return (1);
+}
