eefc4a255dfcf2dbdad2070ec04f796f10647b21
andres98100 <andressolarte580@gmail.com>
2022-12-05 15:40:37 -0500

    read

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * read_textfile - function read a file
+ * @filename: variable char
+ * @letters: variable size_t
+ * Return: size_t
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buff;
+	int fd, read_file, write_file;
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buff = malloc(sizeof(char) * letters + 1);
+	if (!buff)
+		return (0);
+	read_file = read(fd, buff, letters);
+	if (read_file == -1)
+		return (0);
+	buff[letters] = '\0';
+	write_file = write(1, filename, read_file);
+	if (write_file == -1)
+		return (0);
+	close(fd);
+	return (write_file);
+}
