e3136fa416e8d32320192a366111792abb09eb25
andres98100 <andressolarte580@gmail.com>
2022-12-06 10:58:22 -0500

    update add

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	else
 	{
 		new->next = *head;
-		*head = new;
 	}
+	*head = new;
 	return (new);
 }
--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -24,7 +24,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	{
 		while (aux->next)
 			aux = aux->next;
-		aux->next = new;
 	}
+	aux->next = new;
 	return (new);
 }
