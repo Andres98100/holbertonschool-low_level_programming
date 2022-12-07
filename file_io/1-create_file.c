98c2acc07b97b8a71be771a9cc1d73595c07bd1c
andres98100 <andressolarte580@gmail.com>
2022-12-07 11:48:59 -0500

    update created

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,9 +10,10 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int lentext;
+	int lentext = 0;
 
-	lentext = strlen(text_content);
+	if (text_content)
+		lentext = strlen(text_content);
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
