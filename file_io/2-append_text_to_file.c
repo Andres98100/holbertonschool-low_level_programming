7ac370a0538ec5088520cdfc9c4a08a751323145
andres98100 <andressolarte580@gmail.com>
2022-12-08 14:23:56 -0500

    append

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -9,17 +9,17 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd;
-    int lentxt = 0;
+	int fd;
+	int lentxt = 0;
 
-    if (text_content)
-        lentxt = strlen(text_content);
-    if (!filename)
-        return (-1);
-    fd = open(filename, O_WRONLY | O_APPEND);
+	if (text_content)
+		lentxt = strlen(text_content);
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 	write(fd, text_content, lentxt);
-	close (fd);
+	close(fd);
 	return (1);
 }
