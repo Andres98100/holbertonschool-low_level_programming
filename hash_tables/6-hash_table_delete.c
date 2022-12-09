311b633eaa90141ceabbf6e21efc2e4d4faed653
andres98100 <andressolarte580@gmail.com>
2022-12-09 17:39:07 -0500

    delete ht

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - function delete ht
+ * @ht: variable struct
+ * Return: void
+*/
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *aux;
+	unsigned long int i;
+
+	if (!ht)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		aux = ht->array[i];
+		while (aux)
+		{
+			free(aux->key);
+			free(aux->value);
+			free(aux);
+			aux = aux->next;
+		}
+	}
+	free(ht->array);
+	free(aux);
+}
