6278571e74a47916d5bc45b229bd1d8de402824c
andres98100 <andressolarte580@gmail.com>
2022-12-08 19:15:31 -0500

    update

---
+++ file_io/3-cp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - function main
+ * @argc: variable int
+ * @argv: variable char
+ * Return: int
+*/
+
+int main(int argc, char **argv)
+{
+    int fd;
+    char buff[1024];
+}
--- file_io/main.h
+++ file_io/main.h
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <fcntl.h>
 
 /* PROTOTYPES */
