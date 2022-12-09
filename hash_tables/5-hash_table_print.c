8c6761aba9f226548485ee93853642e7ce3e30e3
andres98100 <andressolarte580@gmail.com>
2022-12-09 15:43:08 -0500

    print ht

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function prints a hash table
+ * @ht: variable struct
+ * 
+ * 
+*/
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, aux = 0;
+	hash_node_t *head;
+
+	if (!ht)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		head = ht->array[i];
+		while (head)
+		{
+			if (aux != 0)
+				printf(", ");
+			printf("'%s': '%s'", head->key, head->value);
+			head = head->next;
+			aux++;
+		}
+	}
+	printf("}\n");
+}
