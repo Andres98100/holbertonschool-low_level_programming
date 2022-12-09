a373aa73a474385806d4938e5a0c4342b7125b07
andres98100 <andressolarte580@gmail.com>
2022-12-08 19:18:19 -0500

    update

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -6,9 +6,17 @@
  * @key: variable char
  * @value: variable char
  * Return: int
-*/
+ */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+    unsigned int index;
+    hash_node_t *node = NULL;
 
+    index = hash_djb2(key);
+    node = malloc(sizeof(hash_node_t));
+    node->key = key;
+    node->value = value;
+    node->next = NULL;
+    ht->array[index] = node;
 }
