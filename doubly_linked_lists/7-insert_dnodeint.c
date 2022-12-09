5f995d9e3880235af9df13fb6fcd15f2c6894de8
andres98100 <andressolarte580@gmail.com>
2022-12-09 11:56:41 -0500

    add at index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *aux = *h;
 	unsigned int count = 0;
 
-	if (!*h || (!h && idx != 0))
+	if (!h || (!*h && idx != 0))
 		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
