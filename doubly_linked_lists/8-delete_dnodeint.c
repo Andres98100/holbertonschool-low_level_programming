69bf5cd4e31d4515f7c64b60af96a5a9ef95aff9
andres98100 <andressolarte580@gmail.com>
2022-12-08 19:17:52 -0500

    update

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - function delete at end
+ * @head: variable struct
+ * @index: variable int
+ * Return: int
+*/
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *new;
+}
