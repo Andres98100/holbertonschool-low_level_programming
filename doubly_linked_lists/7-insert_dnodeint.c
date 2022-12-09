02c93c293ad70bcd48c32e35b64f5afbc712afa5
andres98100 <andressolarte580@gmail.com>
2022-12-09 11:58:21 -0500

    add at index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -36,7 +36,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (idx + 1 == idx)
 	{
-		return (add_dnodeint_end(h, n));
+		new->next = NULL;
+		aux->next = new;
+		new->prev = aux;
+		return (new);
 	}
 	free(new);
 	return (NULL);
