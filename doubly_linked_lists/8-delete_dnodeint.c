5462e45098ace62858e46b0864d460539959168f
andres98100 <andressolarte580@gmail.com>
2022-12-09 17:08:36 -0500

    delete

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -8,6 +8,41 @@
 */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *new, *aux = *head;
-	
+	dlistint_t *delete = *head;
+	unsigned int count = 0;
+
+	if (!*head || !head)
+		return (-1);
+	if (index == 0)
+	{
+		if ((*head)->next)
+		{
+			*head = delete->next;
+			(*head)->prev = NULL;
+			free(delete);
+			return (-1);
+		}
+		free(*head);
+		*head = NULL;
+		return (1);
+	}
+	while (delete->next)
+	{
+		if (count == index)
+		{
+			(delete->next)->prev = delete->prev;
+			(delete->prev)->next = delete->next;
+			free(delete);
+			return (1);
+		}
+		count++;
+		delete = delete->next;
+	}
+	if (index == count)
+	{
+		(delete->prev)->next = NULL;
+		free(delete);
+		return (1);
+	}
+	return (-1);
 }
