917cd92829ffd49a45ef35111e829cb931d742d5
andres98100 <andressolarte580@gmail.com>
2022-12-09 19:31:48 -0500

    update delete

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,16 +12,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned int index;
 	hash_node_t *node = NULL;
-	char *dup_key, *dup_value;
+	char *dup_key = NULL, *dup_value = NULL;
 
 	dup_key = strdup(key);
 	dup_value = strdup(value);
-	if (!value || !dup_value)
-	{
-		free(dup_value);
-		return (0);
-	}
-	if (!ht)
+	if (!ht || !value || !dup_value)
 		return (0);
 	index = hash_djb2((const unsigned char *)dup_key) % (ht->size);
 	node = malloc(sizeof(hash_node_t));
@@ -30,6 +25,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node->next = NULL;
 	if (ht->array[index] && strcmp(ht->array[index]->key, dup_key) != 0)
 		node->next = ht->array[index];
+	else if (ht->array[index])
+	{
+		free(ht->array[index]->key);
+		free(ht->array[index]->value);
+		free(ht->array[index]);
+	}
 	ht->array[index] = node;
 	return (1);
 }
