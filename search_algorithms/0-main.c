690ecb0addd13b09003b10723a1cd726cfe510ef
andres98100 <andressolarte580@gmail.com>
2023-01-09 15:09:07 -0500

    solution binary

--- search_algorithms/0-main.c
+++ search_algorithms/0-main.c
@@ -8,12 +8,12 @@
 int main(void)
 {
     int array[] = {
-        10, 1, 42, 3, 4, 42, 6, 7, -1, 9
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9
     };
     size_t size = sizeof(array) / sizeof(array[0]);
 
-    printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
-    printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
-    printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
+    printf("Found %d at index: %d\n\n", 2, binary_search(array, size, 2));
+    printf("Found %d at index: %d\n\n", 5, binary_search(array, 5, 5));
+    printf("Found %d at index: %d\n", 999, binary_search(array, size, 999));
     return (EXIT_SUCCESS);
 }
--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -6,9 +6,33 @@
  * @size: variable size
  * @value: variable int
  * Return: int
-*/
+ */
 
 int binary_search(int *array, size_t size, int value)
 {
-	
+	size_t low = 0, mid = 0, high = size - 1, i;
+
+	if (!array)
+	{
+		return (-1);
+	}
+	while (low <= high)
+	{
+		mid = (high + low) / 2;
+		printf("Searching in array: ");
+		for (i = low; i <= high; i++)
+		{
+			if (i != low)
+				printf(", ");
+			printf("%lu", i);
+		}
+		printf("\n");
+		if (array[mid] == value)
+			return (mid);
+		if (array[mid] < value)
+			low = mid + 1;
+		else
+			high = mid - 1;
+	}
+	return (-1);
 }
